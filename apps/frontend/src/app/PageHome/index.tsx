0a5319e0e1994d496e6bcb7ac56d68341f9fcc15
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2023-07-04 15:38:25 -0700

    Update index.tsx

--- apps/frontend/src/app/PageHome/index.tsx
+++ apps/frontend/src/app/PageHome/index.tsx
@@ -87,6 +87,7 @@ function IntroCard() {
             inventory, and help you create the best build based on how you play,
             with what you have.
           </Trans>
+          asdada
         </Typography>
       </CardContent>
     </CardDark>
