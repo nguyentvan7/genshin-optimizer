2445aa59de7a37b8f010c9e8d3ba75ac0058d012
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2025-02-10 12:17:43 -0700

    Delete test directory

--- test/node_modules/@nx/plugin/src/generators/executor/schema.d.ts
+++
@@ -1,9 +0,0 @@
-export interface Schema {
-  project: string;
-  name: string;
-  description?: string;
-  unitTestRunner: 'jest' | 'none';
-  includeHasher: boolean;
-  skipLintChecks?: boolean;
-  skipFormat?: boolean;
-}
--- test/node_modules/@nx/plugin/src/generators/preset/schema.d.ts
+++
@@ -1,4 +0,0 @@
-export interface PresetGeneratorSchema {
-  pluginName: string;
-  createPackageName?: string;
-}
