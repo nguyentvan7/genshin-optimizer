4dfc6d7ccf3a73dbcff5439ce67b3f4460bde278
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2025-02-10 12:25:43 -0700

    Delete test3 directory

--- test3/node_modules/@nx/vite/src/generators/init/schema.d.ts
+++
@@ -1,6 +0,0 @@
-export interface InitGeneratorSchema {
-  uiFramework: 'react' | 'none';
-  compiler?: 'babel' | 'swc';
-  includeLib?: boolean;
-  testEnvironment?: 'node' | 'jsdom' | 'happy-dom' | 'edge-runtime' | string;
-}
